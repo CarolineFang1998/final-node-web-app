7f2767984f6ee10a8cb120813fa9186809d7d0ac
yingguocs22 <guoyingcs@163.com>
2023-04-07 15:32:38 -0700

    init node

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from 'cors'
+import UsersController from "./users/users-controller.js";
+
+const app = express()
+app.use(cors())
+app.use(express.json());
+
+app.get("/", function (req, res) {
+    res.send("Welcome to Group 27's Final Project!");
+});
+
+app.listen(process.env.PORT || 4000);
