c9ecd02bbcd53b02cdd1fc37d46c99a37e7645e3
CarolineFang1998 <f767475251@gmail.com>
2023-04-08 12:28:00 -0700

    search in yelp API

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from 'cors'
 import UsersController from "./users/users-controller.js";
+import SearchController from "./search/search-controller.js";
 
 const app = express()
 app.use(cors())
@@ -10,4 +11,7 @@ app.get("/", function (req, res) {
     res.send("Welcome to Group 27's Final Project!");
 });
 
-app.listen(process.env.PORT || 4000);
+SearchController(app);
+
+// app.listen(process.env.PORT || 4000);
+app.listen(4000);
---
+++ search/search-controller.js
@@ -0,0 +1,36 @@
+import yelp from 'yelp-fusion';
+
+const apiKey = 'Iv_abp1th6eEephLxCam5MBUu124TXnN0TKd5uAxFSQe3WZtismnqAhRBnAzUBIK5XF3CJIw5NqOxag1XQp'
+               + '7c0fepysR1f0Bdc8ggZa0M9jDyCRzGSjXLYWpUWQjZHYx';
+const client = yelp.client(apiKey);
+
+function SearchController(app) {
+    app.get('/api/search/:location/:param', (req, res) => {
+        const location = req.params.location;
+        const param = req.params.param;
+        client.search({
+                          term: param+'+dog+friendly',
+                          location: location,
+                          limit: '2',
+                          categories: 'restaurants,bars',
+                          open_now: true
+                      }).then(response => {
+            console.log(JSON.stringify(response.jsonBody));
+            res.send(response.jsonBody.businesses);
+        }).catch(e => {
+            console.log(e);
+        });
+    })
+    app.get('/api/detail/:id', (req, res) => {
+        const id = req.params.id;
+        client.business(id).then(response => {
+            console.log(response.jsonBody.name);
+            res.send(response.jsonBody);
+        }).catch(e => {
+            console.log(e);
+        });
+    })
+}
+
+export default SearchController;
+
