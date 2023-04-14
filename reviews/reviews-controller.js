2de0ddce5af40b951b785ccc2ecea0aa09d813c7
moonpiiiiie <cheng.xue910@gmail.com>
2023-04-13 20:05:15 -0700

    initialize reviews model

---
+++ reviews/reviews-controller.js
@@ -0,0 +1,38 @@
+import * from "./reviews-dao.js";
+
+function ReviewsController(app) {
+    const findAllReviews = async (req, res) => {
+        const reviews = await reviewsDao.findAllReviews();
+        res.send(reviews);
+    };
+    const findReviewsByRestaurantId = async (req, res) => {
+        const restaurantId = req.params.restaurantId;
+        const reviews = await reviewsDao.findReviewsByRestaurantId(restaurantId);
+        res.send(reviews);
+    };
+    const findReviewsByUserId = async (req, res) => {
+        const userId = req.params.userId;
+        const reviews = await reviewsDao.findReviewsByUserId(userId);
+        res.send(reviews);
+    };
+    const deleteReview = async (req, res) => {
+        const id = req.params._id;
+        const status = await reviewsDao.deleteReview(id);
+        res.json(status);
+    };
+    const createReview = async (req, res) => {
+        const review = await reviewsDao.createReview(req.body);
+        res.json(review);
+    };
+    const updateReview = async (req, res) => {
+        const id = req.params._id;
+        const status = await reviewsDao.updateReview(id, req.body);
+        res.json(status);
+    };
+    app.get("/api/reviews", findAllReviews);
+    app.get("/api/reviews/:restaurantId", findReviewsByRestaurantId);
+    app.get("/api/reviews/user/:userId", findReviewsByUserId);
+    app.delete("/api/reviews/:_id", deleteReview);
+    app.post("/api/reviews", createReview);
+    app.put("/api/reviews/:_id", updateReview);
+}
---
+++ reviews/reviews-schema.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const reviewsSchema = new mongoose.Schema(
+    {
+        restaurantID: { type: String, required: true },
+        restaurantName: { type: String, required: true },
+        userID: { type: String, required: true },
+        username: { type: String, required: true },
+        review: { type: String, required: true },
+        date: { type: Date, default: Date.now },
+    },
+    {
+        collection: "reviews",
+    }
+);
