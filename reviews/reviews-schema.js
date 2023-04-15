19e2e84f87862ab86f3844470bd46af474289cb0
moonpiiiiie <cheng.xue910@gmail.com>
2023-04-15 15:31:50 -0700

    fix review schema

--- reviews/reviews-schema.js
+++ reviews/reviews-schema.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 const reviewsSchema = new mongoose.Schema(
     {
-        _id: { type: String, required: false },
+       
         restaurantID: { type: String, required: true },
         userID: { type: String, required: true },
         username: { type: String, required: true },
         review: { type: String, required: true },
-        date: { type: Date, default: Date.now }
+        date: { type: Date, default: Date.now() }
     },
     {
         collection: "reviews",
